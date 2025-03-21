dfb28e0cd2a8ce7b4a0cc0643b0ecba8cba5b9f5
VaishnaviHud <146311904+VaishnaviHud@users.noreply.github.com>
2025-03-21 11:02:38 +0530

    added  decoding in h.cpp

--- h.cpp
+++ h.cpp
@@ -6,62 +6,63 @@
 
 using namespace std;
 
+
 struct MinHeapNode {
     char d;
     unsigned frequency;
     MinHeapNode *lChild, *rChild;
 
     MinHeapNode(char d, unsigned frequency) {
-        lChild = rChild = NULL;
+        lChild = rChild = nullptr;
         this->d = d;
         this->frequency = frequency;
     }
 };
 
-// Function to compare nodes
+
 struct compare {
     bool operator()(MinHeapNode *l, MinHeapNode *r) {
         return (l->frequency > r->frequency);
     }
 };
 
-// Helper function to print Huffman codes
-void printCodes(struct MinHeapNode *root, string str, unordered_map<char, string> &huffmanCodes) {
-    if (!root)
-        return;
+
+void generateHuffmanCodes(MinHeapNode *root, string str, unordered_map<char, string> &huffmanCodes) {
+    if (!root) return;
 
     if (root->d != '$') {
         huffmanCodes[root->d] = str;
     }
 
-    printCodes(root->lChild, str + "0", huffmanCodes);
-    printCodes(root->rChild, str + "1", huffmanCodes);
+    generateHuffmanCodes(root->lChild, str + "0", huffmanCodes);
+    generateHuffmanCodes(root->rChild, str + "1", huffmanCodes);
 }
 
-// Function to generate Huffman Codes
-MinHeapNode* HuffmanCodes(char d[], int frequency[], int size, unordered_map<char, string> &huffmanCodes) {
-    struct MinHeapNode *lChild, *rChild, *top;
+
+MinHeapNode* buildHuffmanTree(char d[], int frequency[], int size, unordered_map<char, string> &huffmanCodes) {
     priority_queue<MinHeapNode *, vector<MinHeapNode *>, compare> minHeap;
 
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size; ++i) {
         minHeap.push(new MinHeapNode(d[i], frequency[i]));
+    }
 
     while (minHeap.size() != 1) {
-        lChild = minHeap.top();
+        MinHeapNode *lChild = minHeap.top();
         minHeap.pop();
-        rChild = minHeap.top();
+        MinHeapNode *rChild = minHeap.top();
         minHeap.pop();
-        top = new MinHeapNode('$', lChild->frequency + rChild->frequency);
+
+        MinHeapNode *top = new MinHeapNode('$', lChild->frequency + rChild->frequency);
         top->lChild = lChild;
         top->rChild = rChild;
         minHeap.push(top);
     }
 
-    printCodes(minHeap.top(), "", huffmanCodes);
-    return minHeap.top();
+    MinHeapNode* root = minHeap.top();
+    generateHuffmanCodes(root, "", huffmanCodes);
+    return root;
 }
 
-// Function to read file and calculate frequency of characters
 void calculateFrequency(const string &filename, unordered_map<char, int> &frequencyMap) {
     ifstream file(filename);
     char ch;
@@ -71,15 +72,34 @@ void calculateFrequency(const string &filename, unordered_map<char, int> &freque
     file.close();
 }
 
-// Function to encode the input file using Huffman codes
-void encodeFile(const string &inputFile, const string &outputFile, const unordered_map<char, string> &huffmanCodes) {
+
+void encodeFile(const string &inputFile, const string &encodedFile, const unordered_map<char, string> &huffmanCodes) {
     ifstream inFile(inputFile);
-    ofstream outFile(outputFile);
+    ofstream outFile(encodedFile);
     char ch;
 
     while (inFile.get(ch)) {
-        string code = huffmanCodes.at(ch);
-        outFile << code;
+        outFile << huffmanCodes.at(ch);
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
+
+void decodeFile(const string &encodedFile, const string &decodedFile, MinHeapNode* root) {
+    ifstream inFile(encodedFile);
+    ofstream outFile(decodedFile);
+    char bit;
+    MinHeapNode* curr = root;
+
+    while (inFile.get(bit)) {
+        curr = (bit == '0') ? curr->lChild : curr->rChild;
+
+        if (curr->lChild == nullptr && curr->rChild == nullptr) { // Leaf node found
+            outFile << curr->d;
+            curr = root;
+        }
     }
 
     inFile.close();
@@ -87,14 +107,14 @@ void encodeFile(const string &inputFile, const string &outputFile, const unorder
 }
 
 int main() {
-    string inputFile = "input.txt";  // The file to be encoded
-    string outputFile = "encoded.txt"; // The file to write the encoded output
-
-    // Calculate character frequencies from the input file
+    string inputFile = "input.txt"; 
+    string encodedFile = "encoded.txt"; 
+    string decodedFile = "decoded.txt"; 
+  
     unordered_map<char, int> frequencyMap;
     calculateFrequency(inputFile, frequencyMap);
 
-    // Prepare data for Huffman Coding
+    
     vector<char> characters;
     vector<int> frequencies;
 
@@ -103,17 +123,24 @@ int main() {
         frequencies.push_back(pair.second);
     }
 
+ 
     unordered_map<char, string> huffmanCodes;
-    MinHeapNode* root = HuffmanCodes(characters.data(), frequencies.data(), characters.size(), huffmanCodes);
+    MinHeapNode* root = buildHuffmanTree(characters.data(), frequencies.data(), characters.size(), huffmanCodes);
 
-    // Encode the input file
-    encodeFile(inputFile, outputFile, huffmanCodes);
+ 
+    encodeFile(inputFile, encodedFile, huffmanCodes);
 
-    // Print the Huffman codes
+
+    decodeFile(encodedFile, decodedFile, root);
+
+    
     cout << "Huffman Codes:\n";
     for (const auto &pair : huffmanCodes) {
         cout << pair.first << ": " << pair.second << "\n";
     }
+
+    cout << "\nEncoding completed. Encoded data is stored in '" << encodedFile << "'.";
+    cout << "\nDecoding completed. Decoded output is stored in '" << decodedFile << "'.\n";
 
     return 0;
 }
